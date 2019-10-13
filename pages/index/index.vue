8f0b99e49619fd3a8cba41211d65cebb9b8fd532
C4az6 <1026037632@qq.com>
2019-10-14 04:05:05 +0800

    首页-动态获取轮播图数据

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,11 +1,8 @@
 <template>
 	<view class="page">
 		<swiper :indicator-dots="true" :autoplay="true" class="carousel">
-			<swiper-item>
-				<image src="../../static/carousel/batmanvssuperman.png" class="carousel"></image>
-			</swiper-item>
-			<swiper-item>
-				<image src="../../static/carousel/spiderman.png" class="carousel"></image>
+			<swiper-item v-for="item in carouselList" :key="item.id">
+				<image :src="item.image" class="carousel"></image>
 			</swiper-item>
 		</swiper>
 	</view>
@@ -15,11 +12,28 @@
 	export default {
 		data() {
 			return {
-				
+				baseUrl: 'https://www.imovietrailer.com/superhero',
+				carouselList: []
 			}
 		},
 		onLoad() {
+			uni.request({
+				url: this.baseUrl + '/index/carousel/list',
+				method: 'post',
+				header:{
+					'content-type':'application/x-www-form-urlencoded'
+					},
+				data: {
+					qq: '1335436466'
+				},
+				success: res => {
+					if (res.data.status === 200){
+						this.carouselList = res.data.data
+					}
+				}
 
+
+			});
 		},
 		methods: {
 
@@ -28,5 +42,5 @@
 </script>
 
 <style>
-@import url("index.css");
+	@import url("index.css");
 </style>
--- unpackage/dist/dev/mp-weixin/pages/index/index.js
+++ unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -131,10 +131,7 @@ __webpack_require__.r(__webpack_exports__);
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0; //
-//
-//
-//
+/* WEBPACK VAR INJECTION */(function(uni) {Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0; //
 //
 //
 //
@@ -147,14 +144,32 @@ Object.defineProperty(exports, "__esModule", { value: true });exports.default = 
 var _default =
 {
   data: function data() {
-    return {};
-
+    return {
+      baseUrl: 'https://www.imovietrailer.com/superhero',
+      carouselList: [] };
 
   },
-  onLoad: function onLoad() {
+  onLoad: function onLoad() {var _this = this;
+    uni.request({
+      url: this.baseUrl + '/index/carousel/list',
+      method: 'post',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded' },
+
+      data: {
+        qq: '1335436466' },
+
+      success: function success(res) {
+        if (res.data.status === 200) {
+          _this.carouselList = res.data.data;
+        }
+      } });
+
+
 
   },
   methods: {} };exports.default = _default;
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["default"]))
 
 /***/ }),
 /* 21 */
