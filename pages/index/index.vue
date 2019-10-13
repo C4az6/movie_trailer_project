7b1ea2cc8d8620981c1f1fe97b277c822a5c438f
C4az6 <1026037632@qq.com>
2019-10-14 01:00:01 +0800

    first commit

---
+++ pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="">
+		首页
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
