4d1769ec3e15289546c9d571c9140aac7217eec7
C4az6 <1026037632@qq.com>
2019-10-14 16:52:45 +0800

    搜索页-完成预告片静态布局

--- pages/search/search.vue
+++ pages/search/search.vue
@@ -1,5 +1,53 @@
 <template>
 	<view class="page">
+		<view class="search-block">
+			<view class="search-icon-wapper">
+				<image src="/static/icos/search.png" class="search-ico"></image>
+			</view>
+			<input placeholder="搜索预告" type="text" maxlength="10" class="search-text" focus>
+		</view>
+		
+		<view class="movie-list page-block">
+			<view class="movie-wapper">
+				<image src="http://122.152.205.72:88/superhero/DC/BatManTheDarkKnightRises/cover.jpg" class="poster"></image>
+			</view>
+			<view class="movie-wapper">
+				<image src="http://122.152.205.72:88/superhero/DC/BatManTheDarkKnightRises/cover.jpg" class="poster"></image>
+			</view>
+			<view class="movie-wapper">
+				<image src="http://122.152.205.72:88/superhero/DC/BatManTheDarkKnightRises/cover.jpg" class="poster"></image>
+			</view>
+			<view class="movie-wapper">
+				<image src="http://122.152.205.72:88/superhero/DC/BatManTheDarkKnightRises/cover.jpg" class="poster"></image>
+			</view>
+			<view class="movie-wapper">
+				<image src="http://122.152.205.72:88/superhero/DC/BatManTheDarkKnightRises/cover.jpg" class="poster"></image>
+			</view>
+			<view class="movie-wapper">
+				<image src="http://122.152.205.72:88/superhero/DC/BatManTheDarkKnightRises/cover.jpg" class="poster"></image>
+			</view>
+			<view class="movie-wapper">
+				<image src="http://122.152.205.72:88/superhero/DC/BatManTheDarkKnightRises/cover.jpg" class="poster"></image>
+			</view>
+			<view class="movie-wapper">
+				<image src="http://122.152.205.72:88/superhero/DC/BatManTheDarkKnightRises/cover.jpg" class="poster"></image>
+			</view>
+			<view class="movie-wapper">
+				<image src="http://122.152.205.72:88/superhero/DC/BatManTheDarkKnightRises/cover.jpg" class="poster"></image>
+			</view>
+			<view class="movie-wapper">
+				<image src="http://122.152.205.72:88/superhero/DC/BatManTheDarkKnightRises/cover.jpg" class="poster"></image>
+			</view>
+			<view class="movie-wapper">
+				<image src="http://122.152.205.72:88/superhero/DC/BatManTheDarkKnightRises/cover.jpg" class="poster"></image>
+			</view>
+			<view class="movie-wapper">
+				<image src="http://122.152.205.72:88/superhero/DC/BatManTheDarkKnightRises/cover.jpg" class="poster"></image>
+			</view>
+			<view class="movie-wapper">
+				<image src="http://122.152.205.72:88/superhero/DC/BatManTheDarkKnightRises/cover.jpg" class="poster"></image>
+			</view>
+		</view>
 	</view>
 </template>
 
