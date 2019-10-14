9dfcd633327478392c65902d65d8d806d38d3a89
C4az6 <1026037632@qq.com>
2019-10-14 17:02:27 +0800

    搜索页-完成预告片数据的动态渲染

--- pages/search/search.vue
+++ pages/search/search.vue
@@ -6,46 +6,10 @@
 			</view>
 			<input placeholder="搜索预告" type="text" maxlength="10" class="search-text" focus>
 		</view>
-		
+
 		<view class="movie-list page-block">
-			<view class="movie-wapper">
-				<image src="http://122.152.205.72:88/superhero/DC/BatManTheDarkKnightRises/cover.jpg" class="poster"></image>
-			</view>
-			<view class="movie-wapper">
-				<image src="http://122.152.205.72:88/superhero/DC/BatManTheDarkKnightRises/cover.jpg" class="poster"></image>
-			</view>
-			<view class="movie-wapper">
-				<image src="http://122.152.205.72:88/superhero/DC/BatManTheDarkKnightRises/cover.jpg" class="poster"></image>
-			</view>
-			<view class="movie-wapper">
-				<image src="http://122.152.205.72:88/superhero/DC/BatManTheDarkKnightRises/cover.jpg" class="poster"></image>
-			</view>
-			<view class="movie-wapper">
-				<image src="http://122.152.205.72:88/superhero/DC/BatManTheDarkKnightRises/cover.jpg" class="poster"></image>
-			</view>
-			<view class="movie-wapper">
-				<image src="http://122.152.205.72:88/superhero/DC/BatManTheDarkKnightRises/cover.jpg" class="poster"></image>
-			</view>
-			<view class="movie-wapper">
-				<image src="http://122.152.205.72:88/superhero/DC/BatManTheDarkKnightRises/cover.jpg" class="poster"></image>
-			</view>
-			<view class="movie-wapper">
-				<image src="http://122.152.205.72:88/superhero/DC/BatManTheDarkKnightRises/cover.jpg" class="poster"></image>
-			</view>
-			<view class="movie-wapper">
-				<image src="http://122.152.205.72:88/superhero/DC/BatManTheDarkKnightRises/cover.jpg" class="poster"></image>
-			</view>
-			<view class="movie-wapper">
-				<image src="http://122.152.205.72:88/superhero/DC/BatManTheDarkKnightRises/cover.jpg" class="poster"></image>
-			</view>
-			<view class="movie-wapper">
-				<image src="http://122.152.205.72:88/superhero/DC/BatManTheDarkKnightRises/cover.jpg" class="poster"></image>
-			</view>
-			<view class="movie-wapper">
-				<image src="http://122.152.205.72:88/superhero/DC/BatManTheDarkKnightRises/cover.jpg" class="poster"></image>
-			</view>
-			<view class="movie-wapper">
-				<image src="http://122.152.205.72:88/superhero/DC/BatManTheDarkKnightRises/cover.jpg" class="poster"></image>
+			<view class="movie-wapper" v-for="item in trailerList" :key="item.id">
+				<image :src="item.cover" class="poster"></image>
 			</view>
 		</view>
 	</view>
@@ -55,15 +19,47 @@
 	export default {
 		data() {
 			return {
-				
+				trailerList: []
 			}
 		},
 		methods: {
-			
+
+		},
+		onLoad() {
+			uni.showLoading({
+				mask: true,
+				title: "请稍后..."
+			});
+
+			// 查询猜你喜欢列表
+			uni.request({
+				url: this.baseUrl + '/search/list',
+				method: 'post',
+				header: {
+					'content-type': 'application/x-www-form-urlencoded'
+				},
+				data: {
+					qq: '1335436466',
+					keywords: '',
+					page: '',
+					pageSize: ''
+				},
+				success: res => {
+					if (res.data.status === 200) {
+						this.trailerList = res.data.data.rows
+					}
+				},
+				complete: () => {
+					// 拿到数据后隐藏loading效果
+					uni.hideLoading();
+					// 拿到数据后隐藏刷新效果
+
+				}
+			});
 		}
 	}
 </script>
 
 <style>
-@import url("search.css");
+	@import url("search.css");
 </style>
