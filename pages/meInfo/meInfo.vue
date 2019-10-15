f00142ba61cbbd0c69d404351f9faf64dabdd00c
C4az6 <1026037632@qq.com>
2019-10-16 02:18:50 +0800

    用户设置页面静态布局完成

---
+++ pages/meInfo/meInfo.vue
@@ -0,0 +1,97 @@
+<template>
+	<view class="page page-fill">
+		<view class="page-block info-list">
+			<!-- 头像 -->
+			<view class="item-wapper face-line-upbottom">
+				<view class="info-words">头像</view>
+				
+				<view class="right-wapper">
+					<image src="../../static/icos/default-face.png" class="face"></image>
+					<view class="arrow-block">
+						<image src="../../static/icos/left-gray-arrow.png" class="arrow-ico"></image>
+					</view>
+				</view>
+			</view>
+			
+			<view class="line-top">
+				<view class="line"></view>
+			</view>
+			
+		<!-- 用户名 -->
+			<!-- 头像 -->
+			<view class="item-wapper">
+				<view class="info-words">昵称</view>
+				
+				<view class="right-wapper">
+					<view class="gray-fields">
+						Next学院
+					</view>
+					<view class="arrow-block">
+						<image src="../../static/icos/left-gray-arrow.png" class="arrow-ico"></image>
+					</view>
+				</view>
+			</view>
+			
+			<view class="line-top">
+				<view class="line"></view>
+			</view>
+			
+			<!-- 生日 -->
+			<view class="item-wapper">
+				<view class="info-words">生日</view>
+				
+				<view class="right-wapper">
+					<view class="gray-fields">
+						1997/02/24
+					</view>
+					<view class="arrow-block">
+						<image src="../../static/icos/left-gray-arrow.png" class="arrow-ico"></image>
+					</view>
+				</view>
+			</view>
+			
+			<view class="line-top">
+				<view class="line"></view>
+			</view>
+			<!-- 性别 -->
+			<view class="item-wapper">
+				<view class="info-words">性别</view>
+				
+				<view class="right-wapper">
+					<view class="gray-fields">
+						男
+					</view>
+					<view class="arrow-block">
+						<image src="../../static/icos/left-gray-arrow.png" class="arrow-ico"></image>
+					</view>
+				</view>
+			</view>
+			
+			<view class="line-top">
+				<view class="line"></view>
+			</view>
+		</view>
+		<!-- 底部 -->
+		<view class="footer-wapper">
+			<view class="footer-words">清理缓存</view>
+			<view class="footer-words" style="margin-top: 10upx;">退出登录</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	@import url("meInfo.css");
+</style>
